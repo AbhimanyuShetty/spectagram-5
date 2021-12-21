c5512259ed5b45ccc485940dac175f9019141fdf
AbhimanyuShetty <69106225+AbhimanyuShetty@users.noreply.github.com>
2021-12-21 20:58:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCQSj-Hka9O0xQK_meYs3GaT2nPLEOv9YQ",
+  authDomain: "spectagram-stage-5-final.firebaseapp.com",
+  projectId: "spectagram-stage-5-final",
+  storageBucket: "spectagram-stage-5-final.appspot.com",
+  messagingSenderId: "741606660546",
+  appId: "1:741606660546:web:9b1803b1fa88edea1a1e5b"
+};
